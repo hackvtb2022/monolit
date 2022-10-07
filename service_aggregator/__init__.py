b1e25563acd901a9d9c1436e0e73985c3e1f1d3e
Pavel Kochkin <54332465+pavelkochkin1@users.noreply.github.com>
2022-10-08 00:57:57 +0300

    [UPDATE] добавил ранжирование в импорты аггрегатора

--- service_aggregator/__init__.py
+++ service_aggregator/__init__.py
@@ -1,3 +1,4 @@
+from service_aggregator.cluster_ranking import get_topK_clusters, get_topK_news
 from service_aggregator.clusterization import get_clusters
 from service_aggregator.embedder import (
     embedder_text,
